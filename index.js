564cbea9f3bdf4fcb6bc232836942f03397729b3
Matthew Gaudet <mgaudet@mozilla.com>
2018-11-06 15:05:30 -0500

    Two tables

--- index.js
+++ index.js
@@ -3966,7 +3966,7 @@ class DefaultMap extends Map {
 
 // A map that keeps track of
 let baseDirectoryStats = new DefaultMap(() => 0);
-let filenameDirectoryStats = new DefaultMap(() => 0);
+let filenameStats = new DefaultMap(() => 0);
 
 function create_path_splits() {
     matches.list.forEach(element => {
@@ -3977,21 +3977,31 @@ function create_path_splits() {
         element.filename = pathremainder[pathremainder.length -1];
 
         baseDirectoryStats.set(element.baseDir, baseDirectoryStats.get(element.baseDir) + 1);
-        filenameDirectoryStats.set(element.filename, filenameDirectoryStats.get(element.filename) + 1);
+        filenameStats.set(element.filename, filenameStats.get(element.filename) + 1);
     });
 }
 
-function update_results() {
+function append_two_column_table(target, column1_title, column2_title, map) {
+    let table = document.createElement("table");
+    table.innerHTML = `<tr> <th> ${column1_title}  </th> <th> ${column2_title} </th></tr>`;
+    map.forEach((key, value, _) => {
+        table.innerHTML += `<tr><td>${key}</td><td>${value}</td></tr>`;
+    });
+    target.appendChild(table);
+}
+
+function update_summary() {
     var count = matches.list.length;
-    let res = $("#results");
+    let res = $("#summary");
     let p = document.createElement("p");
     let text = document.createTextNode("Found "+count+" results");
     p.appendChild(text);
     res.appendChild(p);
+
+    append_two_column_table(res, "Base Directory", "Count", baseDirectoryStats);
+    append_two_column_table(res, "Filename", "Count", filenameStats);
 }
 
-update_results();
 create_path_splits();
-
-console.log("Script ran");
-console.log("Found ")
+update_summary();
+console.log("ran scripts");
